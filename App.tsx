223b9030a8e5aad24dc7f5209041e775cc874010
David Basile Filho <39467981+davidbasilefilho@users.noreply.github.com>
2023-08-30 13:45:07 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App(): JSX.Element {
